ff5e5a740598b024300a2148f542d70daa0d37c6
Harisri0103 <93900423+Harisri0103@users.noreply.github.com>
2021-11-30 10:44:37 +0530

    Create switch case for +,-,0(mod 2).c

---
+++ switch case for +,-,0(mod 2).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+int a;
+printf("enter the value of a & \n choose your option ");
+scanf("%d",&a);
+switch(a>0)
+{
+case 1:
+printf("the value is positive");
+break;
+
+case 0:
+switch(a<0)
+case 1 :
+{printf("the value is negative");
+break;
+case 0:
+printf("the value is zero");
+break;
+}}
+return 0;
+
+}
